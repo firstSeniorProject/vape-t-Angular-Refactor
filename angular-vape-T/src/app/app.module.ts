f281bad2fcfd89444cc1694e78dfe0d8f5354440
nounou NOUNOU <brathebow@gmail.com>
2022-11-05 19:50:11 +0100

    purge

--- angular-vape-T/src/app/app.module.ts
+++ angular-vape-T/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,10 +18,10 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-vape-T/src/app/login/login.component.ts
+++ angular-vape-T/src/app/login/login.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
-//import { ConfigService } from './signup.service';
-
-//import { HttpClient } from '@angular/common/http';
-import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormBuilder } from '@angular/forms';
-
-
 
 @Component({
   selector: 'app-login',
@@ -18,12 +13,14 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   public loginForm !: FormGroup;
-  emailvalue!: string;
-  user !: any;
-  
+  // emailvalue!: string;
+   user !: any;
+ 
+  @Input() public parentData:any;
+  @Output() public childEvent:any = new EventEmitter();
+
   constructor(private router: Router, 
-              private http: HttpClient,
-             // private service:ConfigService,
+            private http: HttpClient,
               private formBuilder:FormBuilder,){ }
 
   ngOnInit() {
@@ -35,31 +32,29 @@ export class LoginComponent implements OnInit {
     })
   }
 
-  // login(){
-  //   console.log(this.loginForm.value)
-  //   this.http.post<any>("http://localhost:3000/api/users/login", this.loginForm.value)
-  //   .subscribe(res => {
-  //     alert("Login Up Success");
-  //     this.loginForm.reset();
-  //     this.router.navigate(['/']);
-  // }, err => {alert("Error: Something went wrong " + err.message)})
-  // }
-
   login(){
     console.log(this.loginForm.value)
-    this.http.post<any>("http://localhost:3000/api/users/login", this.loginForm.value)
+    this.http.post<any>("http://localhost:3000/api/users/login", this.loginForm.value )
     .subscribe(res => {
-   //  if(res){
      this.user = res;
         localStorage.setItem('userData',JSON.stringify(this.user));
         console.log(res)
         //alert("Login Up Success");
       this.loginForm.reset();
      this.router.navigate(['/']);
-    // }
-
 
   }, err => {alert("Error: Something went wrong " + err.message)})
+  }
+
+
+
+
+ 
+
+
+
+  fireEvent(){
+    this.childEvent.emit("Hey Hey");
   }
 
  
